2382455080f6ca605d48490c312f387d46328e84
abstrom <abstrm@gmail.com>
2017-04-03 18:43:33 +0200

    Hybrid Newton solver

---
+++ PoissonMYPIIHybridNewton/build/CMakeFiles/poissonPII2lvl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poissonPII2lvl.dir/poissonPII2lvl.cc.o"
+  "poissonPII2lvl.pdb"
+  "poissonPII2lvl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poissonPII2lvl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
